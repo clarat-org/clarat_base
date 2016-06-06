8385e807f098f462ca1f1edc2e3d5f124363ca56
Nils Vollmer <nils_vollmer@gmx.de>
2016-06-06 10:17:17 +0200

    clarat-org/clarat#817 - new under construction state(s) .. needs testing

--- app/models/organization/state_machine.rb
+++ app/models/organization/state_machine.rb
@@ -11,6 +11,9 @@ class Organization
         state :completed
         state :approved
 
+        # Special states object might enter before it is approved
+        state :under_construction_pre # Website under construction pre approve
+
         # Special states object might enter after it was approved
         state :internal_feedback, # There was an issue (internal)
               after_enter: :deactivate_offers!,
@@ -18,8 +21,15 @@ class Organization
         state :external_feedback, # There was an issue (external)
               after_enter: :deactivate_offers!,
               after_exit: :reactivate_offers!
+        state :under_construction_post, # Website under construction post approve
+              after_enter: :deactivate_offers_to_under_construction!,
+              after_exit: :reactivate_offers_from_under_construction!
 
         ## Transitions
+
+        event :reinitialize do
+          transitions from: :under_construction_pre, to: :initialized
+        end
 
         event :complete do
           transitions from: :initialized, to: :completed
@@ -30,6 +40,7 @@ class Organization
           transitions from: :completed, to: :approved # , guard: :different_actor?
           transitions from: :internal_feedback, to: :approved
           transitions from: :external_feedback, to: :approved
+          transitions from: :under_construction_post, to: :approved
         end
 
         event :deactivate_internal do
@@ -40,6 +51,17 @@ class Organization
         event :deactivate_external do
           transitions from: :approved, to: :external_feedback
           transitions from: :internal_feedback, to: :external_feedback
+        end
+
+        event :website_under_construction do
+          # pre approve
+          transitions from: :initialized, to: :under_construction_pre
+          transitions from: :completed, to: :under_construction_pre
+          # post approve
+          transitions from: :approved, to: :under_construction_post
+          # TODO: this might be interesting...
+          transitions from: :internal_feedback, to: :under_construction_post
+          transitions from: :external_feedback, to: :under_construction_post
         end
       end
 
@@ -58,6 +80,27 @@ class Organization
       def reactivate_offers!
         offers.where(aasm_state: 'organization_deactivated').find_each do |o|
           o.approve! if o.may_approve?
+        end
+      end
+
+      # TODO
+      def deactivate_offers_to_under_construction!
+        offers.find_each do |offer|
+          next if offer.website_under_construction!
+          raise "#deactivate_offer_to_under_construction failed for #{offer.id}"
+        end
+      end
+
+      # TODO
+      def reactivate_offers_from_under_construction!
+        # pre-approve offers => re-initialize
+        offers.where(aasm_state: 'under_construction_pre')
+              .find_each(&:reinitialize!)
+        # post-approved offers => approve (if possible)
+        offers.where(aasm_state: 'under_construction_post').find_each do |o|
+          o.approve! if o.may_approve?
+          # next if o.approved?
+          # raise "#reactivate_offer_from_under_construction failed for #{offer.id}"
         end
       end
     end
