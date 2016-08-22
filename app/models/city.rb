d3e65288109ad7fafaa5165a17fb72a00b8d94a3
Nils Vollmer <nils_vollmer@gmx.de>
2016-08-22 12:33:41 +0200

    clarat-org/clarat#891 - created city model and transferred locations data

---
+++ app/models/city.rb
@@ -0,0 +1,9 @@
+# Simple City model - may be extended later.
+class City < ActiveRecord::Base
+  # Associations
+  has_many :locations, inverse_of: :city
+  has_many :offers, through: :locations
+
+  # Validations
+  validates :name, uniqueness: true, presence: true
+end
