eb727dab2abcc58140f4b0c6d4bb039d006a084c
mokus80 <felhoesi@clarat.org>
2017-04-12 10:06:48 +0200

    fix indentations

--- app/models/section_filter.rb
+++ app/models/section_filter.rb
@@ -1,7 +1,7 @@
 class SectionFilter < ActiveRecord::Base
-	has_many :offers
-	has_many :organizations, through: :offers
-	has_many :target_audience_filters
+  has_many :offers
+  has_many :organizations, through: :offers
+  has_many :target_audience_filters
   has_many :divisions, inverse_of: :section_filter
   has_many :categories_section_filters
   has_many :categories, through: :categories_section_filters
