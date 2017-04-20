c591e5ae5648970ac4374cf6de944380bb14ec41
mokus80 <felhoesi@clarat.org>
2017-04-20 11:46:24 +0200

    add section filter tests

--- test/models/section_filter_test.rb
+++ test/models/section_filter_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
-class SectionFilterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe SectionFilter do
+
+  let(:family) { section_filters(:family) }
+  subject { family }
+
+  describe 'attributes' do
+    it { subject.must_respond_to :id }
+    it { subject.must_respond_to :name }
+    it { subject.must_respond_to :identifier }
+  end
+
+  describe '::Base' do
+    describe 'associations' do
+      it { subject.must have_many :offers }
+      it { subject.must have_many(:organizations).through :offers }
+      it { subject.must have_many(:target_audience_filters) }
+      it { subject.must have_many(:divisions) }
+      it { subject.must have_many(:categories_section_filters) }
+      it { subject.must have_many(:categories).through :categories_section_filters}
+    end
+  end
 end
