ece415fd330e5db3890e4de5979a6e1a5d8998c2
Nils Vollmer <nils_vollmer@gmx.de>
2017-05-24 14:44:09 +0200

    clarat-org/clarat#1156 - own connection table for TargetAudienceFilter <-> Offer, moved stamp logic to claradmin and adjusted tests

--- db/migrate/20170521063647_upgrade_filters_offers.rb
+++ db/migrate/20170521063647_upgrade_filters_offers.rb
@@ -1,13 +1,29 @@
 class UpgradeFiltersOffers < ActiveRecord::Migration
   def change
-    add_column :filters_offers, :id, :primary_key
-    add_column :filters_offers, :residency_status, :string
-    add_column :filters_offers, :gender_first_part_of_stamp, :string
-    add_column :filters_offers, :gender_second_part_of_stamp, :string
-    add_column :filters_offers, :age_from, :integer
-    add_column :filters_offers, :age_to, :integer
-    add_column :filters_offers, :age_visible, :boolean, default: false
-    add_column :filters_offers, :created_at, :datetime
-    add_column :filters_offers, :updated_at, :datetime
+    create_table :target_audience_filters_offers, force: true do |t|
+      t.integer "target_audience_filter_id", null: false
+      t.integer "offer_id",  null: false
+      t.string "residency_status"
+      t.string "gender_first_part_of_stamp"
+      t.string "gender_second_part_of_stamp"
+      t.string "addition"
+      t.integer "age_from"
+      t.integer "age_to"
+      t.boolean "age_visible", default: false
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.string "stamp_de"
+      t.string "stamp_en"
+      t.string "stamp_de"
+      t.string "stamp_ar"
+      t.string "stamp_fa"
+      t.string "stamp_fr"
+      t.string "stamp_tr"
+      t.string "stamp_ru"
+      t.string "stamp_pl"
+    end
+
+    add_index "target_audience_filters_offers", ["target_audience_filter_id"], name: "index_ta_filters_offers_on_target_audience_filter_id", using: :btree
+    add_index "target_audience_filters_offers", ["offer_id"], name: "index_target_audience_filters_offers_on_offer_id", using: :btree
   end
 end
