736eddbc81e3349d128baf23f3b426d86e334547
allthegoldt <damjangoldt@Damjans-MacBook-Air.local>
2017-11-03 12:04:40 +0100

    fail_vaidation currently not used

--- app/models/concerns/custom_validatable.rb
+++ app/models/concerns/custom_validatable.rb
@@ -2,10 +2,10 @@ module CustomValidatable
   extend ActiveSupport::Concern
 
   included do
-    def fail_validation field, i18n_selector, options = {}
-      errors.add(field, I18n.t(
-                          "#{self.class.name.downcase}.validations.#{i18n_selector}", options
-      ))
-    end
+    # def fail_validation field, i18n_selector, options = {}
+    #   errors.add(field, I18n.t(
+    #                       "#{self.class.name.downcase}.validations.#{i18n_selector}", options
+    #   ))
+    # end
   end
 end
